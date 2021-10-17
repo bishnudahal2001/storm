780e8d7c2060f587799d498717af7a069ddfbd8e
Bishnu Dahal <koalastoner101@gmail.com>
2021-10-17 01:40:27 -0400

    header update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './head/header/header.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogoComponent } from './head/logo/logo.component';
+import { NavComponent } from './head/nav/nav.component';
 
 
 @NgModule({
@@ -11,10 +14,13 @@ import { LayoutComponent } from './layout/layout.component';
     AppComponent,
     HeaderComponent, 
     LayoutComponent,
+    LogoComponent,
+    NavComponent
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
