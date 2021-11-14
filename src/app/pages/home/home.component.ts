b8fd68506024582845c6e2543f85719f8204a4f6
Bishnu Dahal <koalastoner101@gmail.com>
2021-11-14 16:07:38 -0500

    change

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -35,6 +36,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
